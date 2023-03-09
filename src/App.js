b982b176c351c45ab0ce9d3c69781294ffdcebbd
Tornike1123 <toko36106@gmail.com>
2023-03-09 20:06:01 +0400

    Finish Navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar.jsx";
+import ExerciseDetail from "./pages/ExerciseDetail.jsx";
+import Home from "./pages/Home.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-	return <div className="App">safaÅÍ</div>;
+	return (
+		<Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/exercise/:id" element={<ExerciseDetail />} />
+			</Routes>
+			<Footer />
+		</Box>
+	);
 }
 
 export default App;
